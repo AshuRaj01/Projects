8af2c80867ca458a7730512af04deda2ae8127e8
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2023-01-07 15:36:30 +0530

    Update AtmOperationInterF.java

--- ATM Project/AtmOperationInterF.java
+++ ATM Project/AtmOperationInterF.java
@@ -1,7 +1,7 @@
 package project;
 
 public interface AtmOperationInterF 
-{
+{   //Using Interface(Pure Abstraction)
     public void viewBalance();
     public void withdrawAmount(double withdrawAmount);
     public void depositAmount(double depositAmount);
