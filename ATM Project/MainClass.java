7020d80b098ebb73c79d3ca3673c960d4d4fcd02
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2023-01-07 15:26:26 +0530

    Create MainClass.java

---
+++ ATM Project/MainClass.java
@@ -0,0 +1,12 @@
+package project;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) 
+	{
+         atm_inputs atmip = new atm_inputs();
+         atmip.atm_Inputs();
+	}
+}
