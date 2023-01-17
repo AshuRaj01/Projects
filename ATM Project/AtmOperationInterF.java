666a1e9ee70a4d9ffa1f88388140670932bfb304
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2023-01-17 23:57:24 +0530

    Delete ATM Project directory

--- ATM Project/AtmOperationInterF.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-public interface AtmOperationInterF 
-{   //Using Interface(Pure Abstraction)
-    public void viewBalance();
-    public void withdrawAmount(double withdrawAmount);
-    public void depositAmount(double depositAmount);
-    public void viewMinistatment();
-}
