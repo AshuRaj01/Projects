8040b8540402f9c4c09d7fb22ad1326c95586618
Ashu Raj Srivastav <100837997+AshuRaj01@users.noreply.github.com>
2023-01-17 23:55:14 +0530

    Create AtmOperationInterF.java

---
+++ Java Projects/ATM Project/AtmOperationInterF.java
@@ -0,0 +1,9 @@
+package project;
+
+public interface AtmOperationInterF 
+{   //Using Interface(Pure Abstraction)
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount);
+    public void viewMinistatment();
+}
